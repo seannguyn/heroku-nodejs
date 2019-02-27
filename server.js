245156c809e3fb0874a4809c273cdd0be7bc7e8c
sean <seannguyen5696@gmail.com>
2019-02-27 22:01:19 +1100

    added a procfile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+const port = process.env.PORT || 5000;
 
-app.listen(5000,() => {
+app.listen(port,() => {
     console.log("Starting on port: 5000");
     console.log("lets :D");
 })
