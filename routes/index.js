afce30a74ed54b3d9619ab66184102df2ec247f0
Sean Nguyen <sean.nguyen@tlconsulting.com.au>
2019-03-04 09:25:08 +1100

    pipeline testing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   return res.status(200).json({
-    success: true
+    success: true,
+    index: true
   })
 });
 
